29078ac473f50c1b0676ee339356aae8dc3cdf99
surajnakhale810 <surajnakhale810@gmail.com>
2022-12-22 14:37:18 +0530

    shopping card2

--- script.js
+++ script.js
@@ -6,20 +6,23 @@ let add = document.getElementById("add")
 let total = document.getElementById("total")
 let pricearr = [];
 
-function grandTotal(){
-  let item = itemInput.value
-  let price = (priceInput.value) 
-  pricearr.push( parseInt(price))
-  let sum = 0;
-  if((item !== "") && (price !== "")){
-  let row = document.createElement("tr")
-  row.innerHTML = `<td> ${item} </td> <td> ${price} </td> `
-  table.append(row);
-for(let i=0;i<pricearr.length;i++){
-    sum = sum + pricearr[i];
-}
-  
-  total.innerText = "Grandtotal :" + sum
-}
+function grandTotal() {
+    let item = itemInput.value
+    let price = (priceInput.value)
+    pricearr.push(parseInt(price))
+    let sum = 0;
+    if ((item !== "") && (price !== "")) {
+        let row = document.createElement("tr")
+        row.innerHTML = `<td> ${item} </td> <td> ${price} </td> `
+        table.append(row);
+        for (let i = 0; i < pricearr.length; i++) {
+            sum = sum + pricearr[i];
+        }
+
+        total.innerText = "Grandtotal :" + sum
+        itemInput.value = ""
+        priceInput.value = "" 
+    }
+   
 }
 add.addEventListener('click', grandTotal)
